48422be906bd1e0f7a81887dfa226323131639ef
R-Technology <43551882+Farizal97@users.noreply.github.com>
2021-02-20 17:20:20 +0700

    toko online rocketstore vuejs

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div class="home">
+    <HeaderRocket />
+    <HeroRocket />
+     <WomanRocket />
+     <InstaRocket />
+     <PartnerRocket />
+     <FooterRocket />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderRocket from "@/components/HeaderRocket.vue";
+import HeroRocket from "@/components/HeroRocket.vue";
+import WomanRocket from "@/components/WomanRocket.vue";
+import InstaRocket from "@/components/InstaRocket.vue";
+import PartnerRocket from "@/components/PartnerRocket.vue";
+import FooterRocket from "@/components/FooterRocket.vue";
+
+
+
+export default {
+  name: "home",
+  components: {
+    HeaderRocket,
+    HeroRocket,
+    WomanRocket,
+    InstaRocket,
+    PartnerRocket,
+    FooterRocket
+  }
+};
+</script>
